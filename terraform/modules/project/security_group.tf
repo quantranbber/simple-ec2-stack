e2a0a18396b15d5c25d0d98a7c28f735f7ec3db3
quantranbber <quan.tranbb@gmail.com>
2025-03-02 18:05:14 +0700

    fix ingress lb

--- terraform/modules/project/security_group.tf
+++ terraform/modules/project/security_group.tf
@@ -7,9 +7,9 @@ resource "aws_security_group" "lb_sg" {
 resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress" {
   security_group_id = aws_security_group.lb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = -1
-  to_port           = -1
-  ip_protocol       = "all"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "lb_sg_egress" {
